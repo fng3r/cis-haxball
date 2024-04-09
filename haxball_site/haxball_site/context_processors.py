4786fe493a1815a3061361e01eb3dc9cf657c962
fng3r <dinvk@mail.ru>
2024-04-09 19:16:53 +0500

    order matches by leagues

--- haxball_site/haxball_site/context_processors.py
+++ haxball_site/haxball_site/context_processors.py
@@ -5,8 +5,12 @@ from tournament.models import Match
 
 def running_line_context(request):
     today = timezone.now().today()
-    two_days_ago = today - timezone.timedelta(days=2)
-    latest_matches = Match.objects.filter(is_played=True, match_date__range=[two_days_ago, today]).order_by('-match_date')
-    animation_duration = 20 + 3 * latest_matches.count()
+    three_days_ago = today - timezone.timedelta(days=3)
+    latest_matches = (Match.objects
+                      .filter(is_played=True, match_date__range=[three_days_ago, today])
+                      .order_by('league__priority', 'league__created', '-match_date'))
+    base_duration = 20
+    added_duration = 3 * latest_matches.count()
+    animation_duration = base_duration + added_duration
 
     return {'latest_matches': latest_matches, 'animation_duration': animation_duration}
