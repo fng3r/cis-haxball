41aa7989ea2eaa62ca3b3638296b045c9b1543b2
fng3r <dinvk@mail.ru>
2024-04-09 17:24:16 +0500

    add running line showing latest matches results

---
+++ haxball_site/haxball_site/context_processors.py
@@ -0,0 +1,11 @@
+from django.utils import timezone
+
+from tournament.models import Match
+
+
+def running_line_context(request):
+    today = timezone.now().today()
+    two_days_ago = today - timezone.timedelta(days=2)
+    latest_matches = Match.objects.filter(is_played=True, match_date__range=[two_days_ago, today]).order_by('-match_date')
+
+    return {'latest_matches': latest_matches}
