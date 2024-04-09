c2c031e16a51ff0e85c764dd93e5415ced6c9a1b
fng3r <dinvk@mail.ru>
2024-04-09 18:03:15 +0500

    make animation duration dependent on matches count

--- haxball_site/haxball_site/context_processors.py
+++ haxball_site/haxball_site/context_processors.py
@@ -7,5 +7,6 @@ def running_line_context(request):
     today = timezone.now().today()
     two_days_ago = today - timezone.timedelta(days=2)
     latest_matches = Match.objects.filter(is_played=True, match_date__range=[two_days_ago, today]).order_by('-match_date')
+    animation_duration = 20 + 3 * latest_matches.count()
 
-    return {'latest_matches': latest_matches}
+    return {'latest_matches': latest_matches, 'animation_duration': animation_duration}
