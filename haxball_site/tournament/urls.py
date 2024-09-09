3461df57167f0adb3a6ad90184227f47da18ec76
fng3r <dinvk@mail.ru>
2024-09-09 19:27:58 +0500

    add capability to edit team settings without page reload

--- haxball_site/tournament/urls.py
+++ haxball_site/tournament/urls.py
@@ -2,6 +2,7 @@ from django.urls import path
 
 from .views import (
     DisqualificationsList,
+    EditTeamView,
     FreeAgentList,
     LeagueDetail,
     MatchDetail,
@@ -12,10 +13,10 @@ from .views import (
     TeamRatingView,
     TransfersList,
     cancel_postponement,
-    edit_team_profile,
     halloffame,
     player_detailed_statistics,
     remove_entry,
+    team_statistics,
     update_entry,
 )
 
@@ -32,11 +33,12 @@ urlpatterns = [
     path('free_agents/remove/<int:pk>', remove_entry, name='remove_entry'),
     path('free_agents/update/<int:pk>', update_entry, name='update_entry'),
     path('team/<slug:slug>', TeamDetail.as_view(), name='team_detail'),
-    path('team/<slug:slug>/edit', edit_team_profile, name='edit_team'),
+    path('team/<slug:slug>/edit', EditTeamView.as_view(), name='edit_team'),
     path('teams/', TeamList.as_view(), name='team_list'),
     path('disqualifications', DisqualificationsList.as_view(), name='disqualifications'),
     path('transfers', TransfersList.as_view(), name='transfers'),
     path('<slug:slug>', LeagueDetail.as_view(), name='league'),
     path('match/<int:pk>', MatchDetail.as_view(), name='match_detail'),
     path('player_stats/<int:pk>', player_detailed_statistics, name='player_stats'),
+    path('team_stats/<int:pk>', team_statistics, name='team_stats'),
 ]
