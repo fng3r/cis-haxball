61df4487ededca5b02c31f2c193c5daa1627f86e
fng3r <dinvk@mail.ru>
2024-11-03 07:10:03 +0500

    prefetch comments recursively by given depth

--- haxball_site/core/utils.py
+++ haxball_site/core/utils.py
@@ -7,19 +7,19 @@ from .models import LikeDislike, NewComment
 
 def get_comments_for_object(model, obj_id):
     prefetch_likes = Prefetch(
-        'votes', queryset=LikeDislike.objects.likes().prefetch_related('user__user_profile'), to_attr='likes'
+        'votes',
+        queryset=LikeDislike.objects.likes().prefetch_related('user__user_profile__user_icon'),
+        to_attr='likes'
     )
     prefetch_dislikes = Prefetch(
-        'votes', queryset=LikeDislike.objects.dislikes().prefetch_related('user__user_profile'), to_attr='dislikes'
+        'votes',
+        queryset=LikeDislike.objects.dislikes().prefetch_related('user__user_profile__user_icon'),
+        to_attr='dislikes'
     )
 
     return (
-        NewComment.objects.select_related('author__user_profile')
-        .prefetch_related(
+        prefetch_recursively(
             'author__user_profile__user_icon',
-            'childs__author__user_profile__user_icon',
-            # tweak for prefetching recursive field
-            'childs__childs__childs__childs__childs',
             prefetch_likes,
             prefetch_dislikes,
         )
@@ -31,3 +31,20 @@ def get_paginated_comments(comments, page, per_page=20):
     paginator = Paginator(comments, per_page)
 
     return paginator.get_page(page)
+
+
+def prefetch_recursively(*prefetches, depth=15):
+    qs = NewComment.objects.prefetch_related(*prefetches)
+    
+    current_depth = 1
+    while current_depth < depth:
+        children_prefix = 'childs' + '__childs' * (current_depth - 1)
+        qs = qs.prefetch_related(
+            Prefetch(
+                children_prefix,
+                queryset=NewComment.objects.prefetch_related(*prefetches)
+            )
+        )
+        current_depth += 1
+        
+    return qs
