7a3297da80151e21ffa961add50d86a18084aaa3
fng3r <dinvk@mail.ru>
2024-11-15 11:34:54 +0500

    remove 'is_round_robin' field from Season model

--- haxball_site/tournament/models.py
+++ haxball_site/tournament/models.py
@@ -64,9 +64,6 @@ class Season(models.Model):
     number = models.SmallIntegerField('Номер сезона')
     is_active = models.BooleanField('Текущий')
     created = models.DateTimeField('Создана', auto_now_add=True)
-    is_round_robin = models.BooleanField(
-        'Круговой розыгрыш', help_text='Галочка, если обычный ЧР, если нету - ЛЧ или иже с ним', default=True
-    )
     bound_season = models.ForeignKey(
         'self', verbose_name='Связанный сезон', null=True, blank=True, on_delete=models.SET_NULL
     )
