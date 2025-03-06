792c0976fc0b612581c37069e22e0d422cd5037e
fng3r <dinvk@mail.ru>
2025-03-06 22:03:04 +0500

    add carousel with match results

--- haxball_site/utils/templatetags/utils.py
+++ haxball_site/utils/templatetags/utils.py
@@ -7,3 +7,10 @@ register = template.Library()
 @register.simple_tag
 def media(path):
     return f'{settings.MEDIA_URL}{path}'
+
+@register.filter
+def batch(iterable, batch_size):
+    lst = list(iterable)
+    res = [lst[i:i + batch_size] for i in range(0, len(lst), batch_size)]
+    print(res)
+    return res
