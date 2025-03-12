6bf756c4626c70ed6ef0c3a0d134f0f4d5271e68
fng3r <dinvk@mail.ru>
2025-03-13 01:42:29 +0500

    add current team form to match page

--- haxball_site/tournament/views.py
+++ haxball_site/tournament/views.py
@@ -420,6 +420,32 @@ class MatchDetail(DetailView):
         context['comments'] = comments
         comment_form = NewCommentForm()
         context['comment_form'] = comment_form
+        
+        team_home_latest_matches = (
+            Match.objects.filter(
+                Q(team_home=match.team_home) | Q(team_guest=match.team_home),
+                ~Q(id=match.id),
+                league=match.league,
+                is_played=True
+            )
+            .select_related('team_home', 'team_guest', 'numb_tour', 'league__championship', 'stage', 'group', 'result')
+            .order_by('-match_date', '-numb_tour', '-id').reverse()[:5]
+        )
+        team_guest_latest_matches = (
+            Match.objects.filter(
+                Q(team_home=match.team_guest) | Q(team_guest=match.team_guest),
+                ~Q(id=match.id),
+                league=match.league,
+                is_played=True
+            )
+            .select_related('team_home', 'team_guest', 'numb_tour', 'league__championship', 'stage', 'group', 'result')
+            .order_by('-match_date', '-numb_tour', '-id').reverse()[:5]
+        )
+        
+        context['latest_matches'] = {
+            'team_home': team_home_latest_matches,
+            'team_guest': team_guest_latest_matches,
+        }
 
         all_matches_between = Match.objects.filter(
             Q(team_guest=match.team_guest, team_home=match.team_home, is_played=True)
