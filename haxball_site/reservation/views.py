a3b48c91e35041b44f417d13f3e74c66b96749cc
fng3r <dinvk@mail.ru>
2025-05-16 10:18:09 +0500

    add restriction for creating reservations for two different matches within overlapping time range

--- haxball_site/reservation/views.py
+++ haxball_site/reservation/views.py
@@ -2,11 +2,13 @@ from datetime import datetime, time, timedelta
 
 from django.contrib import messages
 from django.core.paginator import Paginator
+from django.db.models import Q
 from django.shortcuts import get_object_or_404, redirect, render
 from django.urls import reverse
 from django.utils import timezone
 from django.views.decorators.http import require_POST
 from django.views.generic import ListView
+from tournament.models import Match
 
 from .models import Replay, ReservationEntry, ReservationHost
 from .templatetags.reservation_extras import teams_can_reserve
@@ -60,20 +62,42 @@ class ReservationList(ListView):
             datetime.strptime(data['match_date'], '%Y-%m-%d').date(),
             time(hour=int(data['match_hour']), minute=int(data['match_minute'])),
         )
-        prev_match_date = match_date - timedelta(minutes=29)
-        next_match_date = match_date + timedelta(minutes=29)
-
-        reserved = ReservationEntry.objects.filter(
-            time_date__range=[prev_match_date, next_match_date],
-            host_id=host_id,
-            cancelled_at__isnull=True,
+        prev_match_date = match_date - timedelta(minutes=15)
+        next_match_date = match_date + timedelta(minutes=15)
+        
+        match = get_object_or_404(Match, pk=match_id)
+        teams = [match.team_home, match.team_guest]
+        teams_have_other_reservations = (
+            ReservationEntry.objects
+            .filter(
+                Q(match__team_home__in=teams) | Q(match__team_guest__in=teams),
+                time_date__range=[prev_match_date, next_match_date],
+                cancelled_at__isnull=True,
+            )
+            .exists()
         )
-        if not reserved.exists():
+
+        is_host_reserved = (
+            ReservationEntry.objects
+            .filter(
+                time_date__range=[prev_match_date, next_match_date],
+                host_id=host_id,
+                cancelled_at__isnull=True,
+            )
+            .exists()
+        )
+        if is_host_reserved:
+            messages.error(request, 'Выбранное время занято!')
+        elif teams_have_other_reservations:
+            messages.error(
+                request,
+                'Одна из команд уже имеет активную бронь в промежуток ' +
+                f'с {prev_match_date.strftime('%H:%M')} по {next_match_date.strftime('%H:%M')}'
+            )
+        else:
             ReservationEntry.objects.create(
                 author=request.user, time_date=match_date, match_id=match_id, host_id=host_id
             )
-        else:
-            messages.error(request, 'Выбранное время занято!')
 
         return redirect(reverse('reservation:host_reservation'))
 
