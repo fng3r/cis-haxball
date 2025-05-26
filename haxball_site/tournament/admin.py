b81ab42e187ff7d10bbb0420d5cae6355e6a41a2
fng3r <dinvk@mail.ru>
2025-05-26 16:53:52 +0500

    optimize queries for Player model

--- haxball_site/tournament/admin.py
+++ haxball_site/tournament/admin.py
@@ -176,6 +176,9 @@ class PlayerAdmin(UnfoldModelAdmin):
             return ['name']
         
         return []
+    
+    def get_queryset(self, request):
+        return super().get_queryset(request).select_related('name', 'team', 'player_nation')
 
 
 @admin.register(PlayerTransfer)
