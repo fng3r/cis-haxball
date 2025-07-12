2e8fa49ffb2ab20c35651d9076acf204ceeacf99
fng3r <dinvk@mail.ru>
2025-07-12 22:12:34 +0500

    remove empty choice option from prediction forms

--- haxball_site/predictions/forms.py
+++ haxball_site/predictions/forms.py
@@ -1,19 +1,7 @@
 from django import forms
 from django.contrib.auth.models import User
 
-from tournament.models import TourNumber
-
-from .models import Prediction, PredictionSubmission, PredictionTournament
-from .utils import get_open_tours
-
-
-class PredictionForm(forms.ModelForm):
-    """Form for making predictions"""
-
-    class Meta:
-        model = Prediction
-        fields = ['predicted_result']
-        widgets = {'predicted_result': forms.RadioSelect()}
+from .models import PredictionTournament
 
 
 class TournamentFilterForm(forms.Form):
@@ -21,7 +9,7 @@ class TournamentFilterForm(forms.Form):
 
     tournament = forms.ModelChoiceField(
         queryset=PredictionTournament.objects.filter(is_active=True),
-        empty_label='Выберите турнир',
+        empty_label=None,
         label='Турнир',
         required=False,
     )
@@ -32,27 +20,7 @@ class UserFilterForm(forms.Form):
 
     user = forms.ModelChoiceField(
         queryset=User.objects.filter(is_active=True).order_by('username'),
-        empty_label='Выберите пользователя',
+        empty_label=None,
         label='Пользователь',
         required=False,
     )
-
-
-class PredictionSubmissionForm(forms.ModelForm):
-    """Form for creating/updating prediction submissions"""
-
-    class Meta:
-        model = PredictionSubmission
-        fields = ['tour', 'tournament']
-
-    def __init__(self, *args, **kwargs):
-        user = kwargs.pop('user', None)
-        super().__init__(*args, **kwargs)
-
-        if user:
-            # Filter tournaments to only active ones
-            self.fields['tournament'].queryset = PredictionTournament.objects.filter(is_active=True)
-
-            # Filter tours to only those that are open for predictions
-            open_tours = get_open_tours()
-            self.fields['tour'].queryset = TourNumber.objects.filter(id__in=open_tours)
