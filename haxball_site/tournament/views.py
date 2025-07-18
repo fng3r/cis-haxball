115c306b934cdb6fa49de92b84fc8ae76306903b
Nagaev Dmitry <dinvk@mail.ru>
2025-07-18 10:02:16 +0500

    Add caching for Hall of Fame template fragments (#189)
    * add granular caching, fetch teams hall of fame on page load
    
    * add little margin to row stat value

--- haxball_site/tournament/views.py
+++ haxball_site/tournament/views.py
@@ -838,7 +838,7 @@ def players_hall_of_fame(request):
 
     return render(
         request,
-        'tournament/hall_of_fame/partials/players_hall_of_fame.html',
+        'tournament/hall_of_fame/partials/players_hall_of_fame.html#players-content',
         {
             'players_tops': players,
             'players_filter': HallOfFamePlayerFilter(request.GET, queryset=Player.objects.none()),
@@ -885,7 +885,7 @@ def teams_hall_of_fame(request):
 
     return render(
         request,
-        'tournament/hall_of_fame/partials/teams_hall_of_fame.html',
+        'tournament/hall_of_fame/partials/teams_hall_of_fame.html#teams-content',
         {
             'teams_tops': teams,
             'teams_filter': HallOfFameTeamFilter(request.GET, queryset=Team.objects.none()),
