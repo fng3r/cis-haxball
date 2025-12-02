0f70a1a417498a9aadfe4a72c59c42a7c2187f2f
fng3r <dinvk@mail.ru>
2025-12-02 18:06:59 +0500

    add Celery and Django Celery extensions for task management

--- haxball_site/haxball_site/admin.py
+++ haxball_site/haxball_site/admin.py
@@ -1,10 +1,24 @@
+from django.contrib import admin
 from django.contrib.postgres.fields import ArrayField
 
+from django_celery_beat.admin import ClockedScheduleAdmin as BaseClockedScheduleAdmin
+from django_celery_beat.admin import CrontabScheduleAdmin as BaseCrontabScheduleAdmin
+from django_celery_beat.admin import PeriodicTaskAdmin as BasePeriodicTaskAdmin
+from django_celery_beat.admin import PeriodicTaskForm, TaskSelectWidget
+from django_celery_beat.models import (
+    ClockedSchedule,
+    CrontabSchedule,
+    IntervalSchedule,
+    PeriodicTask,
+    SolarSchedule,
+)
+from django_celery_results.admin import TaskResultAdmin as BaseTaskResultAdmin
+from django_celery_results.models import TaskResult
 from smart_selects.db_fields import ChainedForeignKey
 from smart_selects.widgets import ChainedSelect
 from unfold import admin as unfold_admin
 from unfold.contrib.forms.widgets import ArrayWidget
-from unfold.widgets import SELECT_CLASSES
+from unfold.widgets import SELECT_CLASSES, UnfoldAdminSelectWidget, UnfoldAdminTextInputWidget
 
 
 class UnfoldChainedSelect(ChainedSelect):
@@ -54,4 +68,55 @@ class UnfoldStackedInline(ChainedForeignKeySupportMixin, unfold_admin.StackedInl
 
 
 class UnfoldTabularInline(ChainedForeignKeySupportMixin, unfold_admin.TabularInline):
+    pass
+
+
+admin.site.unregister(PeriodicTask)
+admin.site.unregister(IntervalSchedule)
+admin.site.unregister(CrontabSchedule)
+admin.site.unregister(SolarSchedule)
+admin.site.unregister(ClockedSchedule)
+
+
+class UnfoldTaskSelectWidget(UnfoldAdminSelectWidget, TaskSelectWidget):
+    pass
+
+
+class UnfoldPeriodicTaskForm(PeriodicTaskForm):
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+        self.fields['task'].widget = UnfoldAdminTextInputWidget()
+        self.fields['regtask'].widget = UnfoldTaskSelectWidget()
+
+
+@admin.register(PeriodicTask)
+class PeriodicTaskAdmin(BasePeriodicTaskAdmin, UnfoldModelAdmin):
+    form = UnfoldPeriodicTaskForm
+
+
+@admin.register(IntervalSchedule)
+class IntervalScheduleAdmin(UnfoldModelAdmin):
+    pass
+
+
+@admin.register(CrontabSchedule)
+class CrontabScheduleAdmin(BaseCrontabScheduleAdmin, UnfoldModelAdmin):
+    pass
+
+
+@admin.register(SolarSchedule)
+class SolarScheduleAdmin(UnfoldModelAdmin):
+    pass
+
+
+@admin.register(ClockedSchedule)
+class ClockedScheduleAdmin(BaseClockedScheduleAdmin, UnfoldModelAdmin):
+    pass
+
+
+admin.site.unregister(TaskResult)
+
+
+@admin.register(TaskResult)
+class TaskResultAdmin(BaseTaskResultAdmin, UnfoldModelAdmin):
     pass
