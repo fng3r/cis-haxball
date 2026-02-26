a65604a78089f50444b6136d4db4ace5b9027adc
Nagaev Dmitry <dinvk@mail.ru>
2026-02-26 08:04:00 +0500

    Add multiseason support for fantasy and predictions pages (#222)

--- haxball_site/predictions/forms.py
+++ haxball_site/predictions/forms.py
@@ -1,20 +1,42 @@
 from django import forms
 from django.contrib.auth.models import User
 
-from tournament.models import League, TourNumber
+from tournament.models import League, Season, TourNumber
 
 from .models import PredictionsContestTournament, PreseasonPredictionsTournament
+
+
+class SeasonFilterForm(forms.Form):
+    """Form for filtering by season."""
+
+    season = forms.ModelChoiceField(
+        queryset=Season.objects.none(),
+        empty_label=None,
+        label='Сезон',
+        required=False,
+    )
+
+    def __init__(self, available_seasons, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+        self.fields['season'].queryset = available_seasons
 
 
 class TournamentFilterForm(forms.Form):
     """Form for filtering by tournament"""
 
     tournament = forms.ModelChoiceField(
-        queryset=PredictionsContestTournament.objects.filter(is_active=True).order_by('league__priority'),
+        queryset=PredictionsContestTournament.objects.none(),
         empty_label=None,
         label='Турнир',
         required=False,
     )
+
+    def __init__(self, *args, season: Season | None = None, **kwargs):
+        super().__init__(*args, **kwargs)
+        queryset = PredictionsContestTournament.objects.all()
+        if season is not None:
+            queryset = queryset.filter(league__championship=season)
+        self.fields['tournament'].queryset = queryset.order_by('league__priority')
 
 
 class TourFilterForm(forms.Form):
@@ -37,11 +59,18 @@ class PreseasonPredictionsTournamentFilterForm(forms.Form):
     """Form for filtering by preseason predictions tournament"""
 
     tournament = forms.ModelChoiceField(
-        queryset=PreseasonPredictionsTournament.objects.order_by('league__priority'),
+        queryset=PreseasonPredictionsTournament.objects.none(),
         empty_label=None,
         label='Турнир',
         required=False,
     )
+
+    def __init__(self, *args, season: Season | None = None, **kwargs):
+        super().__init__(*args, **kwargs)
+        queryset = PreseasonPredictionsTournament.objects.all()
+        if season is not None:
+            queryset = queryset.filter(league__championship=season)
+        self.fields['tournament'].queryset = queryset.order_by('league__priority')
 
 
 class UserFilterForm(forms.Form):
