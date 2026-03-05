051f0c39fb9132a9a32640eec9c7659385befe7f
fng3r <dinvk@mail.ru>
2026-03-05 05:58:04 +0500

    add command options to filter matches by season and match IDs

--- haxball_site/tournament/management/commands/build_match_stats.py
+++ haxball_site/tournament/management/commands/build_match_stats.py
@@ -1,5 +1,3 @@
-import time
-
 from django.core.management.base import BaseCommand
 
 from tournament.tasks import fetch_match_replay_stats
@@ -11,12 +9,30 @@ class Command(BaseCommand):
     help = 'Build match stats'
 
     def add_arguments(self, parser):
-        pass
+        parser.add_argument(
+            '--season',
+            type=int,
+            default=None,
+            help='Season number (championship.number) to filter matches',
+        )
+        parser.add_argument(
+            '--match-ids',
+            type=int,
+            nargs='+',
+            default=None,
+            help='List of match IDs to process',
+        )
 
     def handle(self, *args, **options):
-        matches = Match.objects.filter(league__championship__number=22)
+        season_number = options.get('season')
+        match_ids = options.get('match_ids')
+
+        matches = Match.objects.all().order_by('id')
+        if season_number is not None:
+            matches = matches.filter(league__championship__number=season_number)
+        if match_ids:
+            matches = matches.filter(id__in=match_ids)
+
+        self.stdout.write(f'{matches.count()} matches will be processed')
         for match in matches:
-            if match.id == 6124:
-                fetch_match_replay_stats.delay(match.id)
-                time.sleep(5)
-        print(f'{len(matches)} matches will be processed')
+            fetch_match_replay_stats.delay(match.id)
