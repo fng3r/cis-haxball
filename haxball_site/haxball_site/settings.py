2703cac230aae9573d16ac16c121f5608efc1fb5
Nagaev Dmitry <dinvk@mail.ru>
2026-04-01 00:24:02 +0500

    Django 6 (#231)
    * upgrade to Django 6
    
    * remove obsolete/transitive deps from dependencies list
    
    * add ALLAUTH_TRUSTED_CLIENT_IP_HEADER setting to make app work properly behind nginx
    
    * update secondary deps
    
    * upgrade celery[redis]
    
    * upgrade sorl-thumbnail
    
    * rm null=True from ManyToManyField's

--- haxball_site/haxball_site/settings.py
+++ haxball_site/haxball_site/settings.py
@@ -18,7 +18,6 @@ DEBUG = config('APP_DEBUG', cast=bool, default=False)
 ALLOWED_HOSTS = config('APP_ALLOWED_HOSTS', cast=str.split)
 
 INSTALLED_APPS = [
-    'template_partials',
     'online_users',
     'notifications',
     'django_filters',
@@ -31,6 +30,7 @@ INSTALLED_APPS = [
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
+    'django.contrib.postgres',
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
@@ -101,7 +101,6 @@ TEMPLATES = [
                 'haxball_site.context_processors.themes_context',
                 'haxball_site.context_processors.settings_context',
             ],
-            'builtins': ['template_partials.templatetags.partials'],
         },
     },
 ]
@@ -628,3 +627,5 @@ CELERY_ACCEPT_CONTENT = ['json']
 CELERY_TASK_SERIALIZER = 'json'
 CELERY_RESULT_SERIALIZER = 'json'
 CELERY_BEAT_SCHEDULER = 'django_celery_beat.schedulers:DatabaseScheduler'
+
+ALLAUTH_TRUSTED_CLIENT_IP_HEADER = 'X-Real-IP'
