108e8b524452af95ddf85415e899952944066c8b
fng3r <dinvk@mail.ru>
2026-03-31 23:41:39 +0500

    all ALLAUTH_TRUSTED_CLIENT_IP_HEADER setting to make app work properly behind nginx

--- haxball_site/haxball_site/settings.py
+++ haxball_site/haxball_site/settings.py
@@ -627,3 +627,5 @@ CELERY_ACCEPT_CONTENT = ['json']
 CELERY_TASK_SERIALIZER = 'json'
 CELERY_RESULT_SERIALIZER = 'json'
 CELERY_BEAT_SCHEDULER = 'django_celery_beat.schedulers:DatabaseScheduler'
+
+ALLAUTH_TRUSTED_CLIENT_IP_HEADER = 'X-Real-IP'
